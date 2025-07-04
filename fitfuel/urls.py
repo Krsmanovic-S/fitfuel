ad42e1d482576e5e68a8715be7eaf6952f156e4f
Stefan Krsmanovic <103185975+Krsmanovic-S@users.noreply.github.com>
2025-07-05 01:58:30 +0200

    Update urls.py

--- fitfuel/urls.py
+++ fitfuel/urls.py
@@ -8,4 +8,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('food.urls')),
     path('users/', include('users.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
